cc7e4339360cdb0d7d40c3f20fbfb5948cdf4f1e
Shashank Gupta <shashank.sj_2006@yahoo.co.in>
2026-01-24 17:00:25 +0530

    fix token generation for api test

--- cypress/e2e/api/spotify.api.cy.ts
+++ cypress/e2e/api/spotify.api.cy.ts
@@ -1,22 +1,29 @@
 describe('Spotify API Tests', () => {
+  let spotifyToken;
+
+  before('before all tests', () => {
+    // Generate token before all test and store in an alias
+    cy.generateSpotifyToken().then((token: string) => {
+      spotifyToken = token;
+    });
+  });
+
   it('get new releases', () => {
-    cy.generateSpotifyToken().then((token) => {
-      cy.request({
-        method: 'GET',
-        url: `https://api.spotify.com/v1/browse/new-releases`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        qs: {
-          limit: 3,
-        },
-      }).then((response) => {
-        expect(response.status).eq(200);
-        const responseBody = response.body;
-        cy.log(`Response = ${JSON.stringify(responseBody)}`);
-        expect(responseBody).to.not.be.empty;
-        // TODO - Add data assertions
-      });
+    cy.request({
+      method: 'GET',
+      url: `https://api.spotify.com/v1/browse/new-releases`,
+      headers: {
+        Authorization: `Bearer ${spotifyToken}`,
+      },
+      qs: {
+        limit: 3,
+      },
+    }).then((response) => {
+      expect(response.status).eq(200);
+      const responseBody = response.body;
+      cy.log(`Response = ${JSON.stringify(responseBody)}`);
+      expect(responseBody).to.not.be.empty;
+      // TODO - Add data assertions
     });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,28 +2,22 @@ Cypress.Commands.add('generateSpotifyToken', () => {
   const clientId = Cypress.env('spotifyClientId');
   const clientSecret = Cypress.env('spotifyClientSecret');
 
-  expect(clientId).to.not.be.empty;
-  expect(clientSecret).to.not.be.empty;
+  if (!clientId || !clientSecret) {
+    throw new Error('Credentials are not set, please check!');
+  }
 
-  cy.get('@spotifyToken', { timeout: 0 }).then((token) => {
-    if (token) {
+  return cy
+    .request({
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      form: true,
+      body: { grant_type: 'client_credentials' },
+      headers: {
+        Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+      },
+    })
+    .then((response) => {
+      const token = response.body.access_token;
       return token;
-    } else {
-      return cy
-        .request({
-          method: 'POST',
-          url: 'https://accounts.spotify.com/api/token',
-          form: true,
-          body: { grant_type: 'client_credentials' },
-          headers: {
-            Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-          },
-        })
-        .then((response) => {
-          const token = response.body.access_token;
-          cy.wrap(token, { log: false }).as('spotifyToken');
-          return token;
-        });
-    }
-  });
+    });
 });
